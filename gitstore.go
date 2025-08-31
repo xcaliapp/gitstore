f13d3391a28aa0e10c1eb4a1da47bbdfd70740f3
Kovács Péter <peter.dunay.kovacs@gmail.com>
2025-08-31 23:12:55 +0200

    Use random ids for drawings instead of encoded titles

--- gitstore.go
+++ gitstore.go
@@ -2,6 +2,7 @@ package gitstore
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"vcblobstore"
@@ -14,35 +15,64 @@ type LocalGitStore struct {
 	repo *local.Git
 }
 
-func (store *LocalGitStore) PutDrawing(ctx context.Context, title string, contentReader io.Reader, modifiedBy string) error {
+func (store *LocalGitStore) PutDrawing(ctx context.Context, key string, contentReader io.Reader, modifiedBy string) error {
 	content, readErr := io.ReadAll(contentReader)
 	if readErr != nil {
-		return fmt.Errorf("failed to read content for %s: %w", title, readErr)
+		return fmt.Errorf("failed to read content for %s: %w", key, readErr)
 	}
 	blobInfo := vcblobstore.BlobInfo{
-		Key:        title,
+		Key:        key,
 		Content:    content,
 		ModifiedBy: modifiedBy,
 	}
 	return store.repo.AddBlob(ctx, blobInfo)
 }
 
-func (store *LocalGitStore) CopyDrawing(ctx context.Context, sourceTitle string, destinationTitle string, modifiedBy string) error {
-	return store.repo.CopyBlob(ctx, sourceTitle, destinationTitle, modifiedBy)
+func (store *LocalGitStore) CopyDrawing(ctx context.Context, sourcekey string, destinationkey string, modifiedBy string) error {
+	return store.repo.CopyBlob(ctx, sourcekey, destinationkey, modifiedBy)
 }
 
 func (store *LocalGitStore) DeleteDrawing(ctx context.Context, title string, modifiedBy string) error {
 	return store.repo.DeleteBlob(ctx, title, modifiedBy)
 }
 
-func (store *LocalGitStore) ListDrawingTitles(ctx context.Context) ([]string, error) {
-	return store.repo.ListBlobKeys(ctx)
+func (store *LocalGitStore) ListDrawings(ctx context.Context) (map[string]string, error) {
+	keys, err := store.repo.ListBlobKeys(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	drawingList := map[string]string{}
+	for _, key := range keys {
+		content, err := store.GetDrawing(ctx, key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get drawing %s: %w", key, err)
+		}
+
+		var drawingContent map[string]any
+		err = json.Unmarshal([]byte(content), &drawingContent)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal drawing %s: %w", key, err)
+		}
+
+		titleAny, ok := drawingContent["title"]
+		if !ok {
+			return nil, fmt.Errorf("drawing title not found for %s", key)
+		}
+		title, cast := titleAny.(string)
+		if !cast {
+			return nil, fmt.Errorf("title of %s is not a string: '%T'", key, titleAny)
+		}
+		drawingList[key] = title
+	}
+
+	return drawingList, nil
 }
 
-func (store *LocalGitStore) GetDrawing(ctx context.Context, title string) (string, error) {
-	blob, err := store.repo.GetBlob(ctx, title)
+func (store *LocalGitStore) GetDrawing(ctx context.Context, key string) (string, error) {
+	blob, err := store.repo.GetBlob(ctx, key)
 	if err != nil {
-		return "", fmt.Errorf("failed to read drawing %s: %w", title, err)
+		return "", fmt.Errorf("failed to read drawing %s: %w", key, err)
 	}
 	return string(blob), nil
 }
